0c0c35eeb222f2d46189771578cb04507a1ccaae
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-05-06 10:09:06 -0600

    job management now works as intended

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -4,8 +4,6 @@ const GROW = "grow";
 const WEAKEN = "weaken";
 const WEAKEN_STRENGTH = 0.05;
 
-// TODO: somethingg isn't quite working right. use send-orders to analyze a single server's threads at a time instead of the whole botnet at once
-
 /** @param {NS} ns */
 export async function main(ns) {
     let threadpool = new_threadpool()
@@ -15,9 +13,14 @@ export async function main(ns) {
         let rservers = network.rootedServers;
         let nservers = network.networkServers;
         let workers = pservers.concat(rservers);
+
+        // clear threadcount for rebuild
+        threadpool.availableThreads = 0;
+        threadpool.totalThreads = 0;
         for (const worker of workers) {
-            add_worker(ns, threadpool, worker);
+            update_worker(ns, threadpool, worker);
         }
+
         for (const target of nservers) {
             add_target(ns, threadpool, target);
         }
@@ -92,10 +95,8 @@ function calculate_optimal_targets(ns, targets, topx) {
     
     // sort descending
     viableTargets.sort(function(a, b){return b.maxMoney - a.maxMoney});
-    // ns.tprint("Top "+topx+" targets are: "+viableTargets.slice(0,topx).map(e => e.server));
     if (topx > viableTargets.length) {
         topx = viableTargets.length;
-        // ns.tprint("There are not enough targets. Capping at "+topx+" instead");
     }
     // strip out the maxMoney parameter and return just the servers
     return viableTargets.slice(0,topx).map(e => e.server);
@@ -115,10 +116,9 @@ function determine_assignment(ns, threadpool) {
     // pick a target
     // TODO: make this updatable with a port read
     let optimalTargets = calculate_optimal_targets(ns, threadpool.targets, 20);
-    // let optimalTargets = ["n00dles"];
+
     // pick an action
     for (const target of optimalTargets) {
-        // ns.tprint("Dispatching for target: "+target);
         let moneyThresh = ns.getServerMaxMoney(target) * 0.9;
         let securityThresh = ns.getServerMinSecurityLevel(target) + 5;
         let moneyAvailable = ns.getServerMoneyAvailable(target);
@@ -126,18 +126,6 @@ function determine_assignment(ns, threadpool) {
         let gthreads = Math.ceil(ns.growthAnalyze(target, Math.ceil((moneyThresh+1)/(moneyAvailable+1))));
         let hthreads = Math.ceil(ns.hackAnalyzeThreads(target, Math.ceil(moneyThresh * 0.5)));
         let allocations = threadpool.allocations[target];
-
-        // ns.tprint("----- ASSIGNMENT -----");
-        // ns.tprint("----- BEFORE -----")
-        // ns.tprint("Target: "+target);
-        // ns.tprint("Total available threads: "+threadpool.availableThreads);
-        // ns.tprint("Threads needed to weaken: "+wthreads);
-        // ns.tprint("Threads needed to grow: "+gthreads);
-        // ns.tprint("Threads needed to hack: "+hthreads);
-        // ns.tprint("-----");
-        // ns.tprint("Threads weakening "+target+": "+allocations.weaken);
-        // ns.tprint("Threads growing "+target+": "+allocations.grow);
-        // ns.tprint("Threads hacking "+target+": "+allocations.hack);
 
         // if not enough threads allocated
         // if above the security threshold
@@ -153,44 +141,31 @@ function determine_assignment(ns, threadpool) {
         if (allocations.hack < hthreads) {
             dispatch(ns, threadpool, HACK, target, hthreads - allocations.hack);
         }
-
-        // ns.tprint("----- AFTER -----")
-        // ns.tprint("Threads weakening "+target+": "+allocations.weaken);
-        // ns.tprint("Threads growing "+target+": "+allocations.grow);
-        // ns.tprint("Threads hacking "+target+": "+allocations.hack);
     }
 }
 
 /** @param {NS} ns */
 function dispatch(ns, threadpool, action, target, threads) {
 
-    // ns.tprint("----- DISPATCH -----");
-    // ns.tprint("Requested threads: "+threads);
     // don't allocate more than 5% resources to a single dispatch
     let maxAllocation = Math.ceil(threadpool.totalThreads * 0.05);
-    // ns.tprint("Max allocation: "+maxAllocation);
+
     // the allocation is the smallest of the request, the max, and the available
     let allocation = Math.min(threads, maxAllocation, threadpool.availableThreads);
-    // ns.tprint("Actual allocation: "+allocation);
+
     // filter for workers with threads remaining
     let availableWorkers = [];
     for (const worker in threadpool.workers) {
-        // ns.tprint(worker);
         if (threadpool.workers[worker].available > 0) {
             availableWorkers.push(worker);
         }
     }
-    // let availableWorkers = new Map([...threadpool.workers].filter(([k,v]) => v > 0));
-    // ns.tprint(availableWorkers);
 
     let i = 0;
     while (allocation > 0 && i < availableWorkers.length) {
         let worker = availableWorkers[i];
         let availableThreads = threadpool.workers[worker].available
 
-        // ns.tprint("----- ALLOCATION -----");
-        // ns.tprint("Worker: "+worker);
-        // ns.tprint("Available threads: "+availableThreads);
         // if the allocation is smaller, then we're done. Exec and break out.
         if (allocation <= availableThreads) {
             assign_worker_threads(ns, threadpool, action, worker, target, allocation);
@@ -199,7 +174,6 @@ function dispatch(ns, threadpool, action, target, threads) {
         // if the available threads is smaller, use em all up and loop to the next worker
         } else {
             assign_worker_threads(ns, threadpool, action, worker, target, availableThreads);
-            // threadpool.workers[worker].available = 0;
             allocation -= availableThreads;
             i += 1;
         }
@@ -219,50 +193,28 @@ function monitor_jobs(ns, threadpool) {
                 ns.toast(job.action+" job completed on "+job.worker+" with target: "+target);
                 finishedJobs.push(job);
             }
-
-            // ns.tprint("-----");
-            // ns.tprint("i: "+i);
-            // ns.tprint("worker: "+job.worker);
-            // ns.tprint(target);
-            // ns.tprint("Remaining: "+remaining);
-            // ns.tprint("Job: "+job.action);
-            // ns.tprint(job.action+" threads: "+threadpool.allocations[target][job.action]);
         }
         for (let job of finishedJobs) {
             remove_job(ns, threadpool, target, job);
         }
-        // if (threadpool.allocations[target].jobs.length == 0) {
-        //     ns.toast("no more jobs. Exiting");
-        //     ns.exit();
-        // }
     }
 }
 
 /** @param {NS} ns */
-function add_worker(ns, threadpool, worker) {
+function update_worker(ns, threadpool, worker) {
     let maxRam = ns.getServerMaxRam(worker);
+    let totalThreads = Math.floor(maxRam / ACTION_COST);
     if (!(worker in threadpool.workers)) {
-        let maxThreads = Math.floor(maxRam / ACTION_COST);
-        ns.tprint("New worker: "+worker+" with threads: "+maxThreads);
-        threadpool.workers[worker] = {max: maxThreads, available: maxThreads};
-        threadpool.totalThreads += maxThreads;
-        threadpool.availableThreads += maxThreads;
+        ns.tprint("New worker: "+worker+" with threads: "+totalThreads);
     }
 
-    if (worker.substring(0,5) == "pserv") {
-        let oldPservMaxThreads = threadpool.workers[worker].max;
-        let oldPservAvailableThreads = threadpool.workers[worker].available;
-        let oldPservThreadsInUse = oldPservMaxThreads - oldPservAvailableThreads;
-        let pservMaxThreads = Math.floor(maxRam / ACTION_COST);
-        if (pservMaxThreads > oldPservMaxThreads) {
-            // server has been updated
-            ns.tprint("New worker: "+worker+" with threads: "+pservMaxThreads);
-            threadpool.workers[worker].max = pservMaxThreads;
-            threadpool.workers[worker].available = pservMaxThreads - oldPservThreadsInUse;
-            threadpool.totalThreads += pservMaxThreads - oldPservMaxThreads;
-            threadpool.availableThreads += pservMaxThreads - oldPservThreadsInUse;
-        }
-    }
+    let usedRam = ns.getServerUsedRam(worker);
+    let availableRam = maxRam - usedRam;
+    let availableThreads = Math.floor(availableRam / ACTION_COST);
+    threadpool.workers[worker] = {max: totalThreads, available: availableThreads};
+
+    threadpool.availableThreads += availableThreads;
+    threadpool.totalThreads += totalThreads;
 }
 
 /** @param {NS} ns */
@@ -291,19 +243,8 @@ function remove_job(ns, threadpool, target, job) {
     let threads = job.threads;
     let index = threadpool.allocations[target].jobs.indexOf(job);
 
-    ns.tprint("----- JOB REMOVE -----");
-    ns.tprint("Worker: "+worker);
-    ns.tprint("Target: "+target);
-    ns.tprint("Action: "+action);
-    ns.tprint("Threads: "+threads);
-    ns.tprint("Total threads before removal: "+threadpool.availableThreads);
-
     threadpool.allocations[target].jobs.splice(index, 1);
     threadpool.allocations[target][action] -= threads;
-    threadpool.workers[worker].available += threads;
-    threadpool.availableThreads += threads;
-
-    ns.tprint("Total threads after removal: "+threadpool.availableThreads);
 }
 
 
@@ -323,16 +264,8 @@ function add_job(ns, threadpool, action, worker, target, threads) {
     }
     let job = {action: action, worker:worker, threads: threads, startTime: performance.now(), duration: actionTime};
 
-    // ns.tprint("----- JOB ADD -----");
-    // ns.tprint("Worker: "+worker);
-    // ns.tprint("Target: "+target);
-    // ns.tprint("Action: "+action);
-    // ns.tprint("Threads: "+threads);
-
     threadpool.allocations[target].jobs.push(job);
     threadpool.allocations[target][action] += threads;
-    threadpool.workers[worker].available -= threads;
-    threadpool.availableThreads -= threads;
 }
 
 function new_threadpool() {
