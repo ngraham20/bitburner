1bf6c103e488e958a15e7dd92450b06b5cce0c11
Nathaniel Graham <nathaniel.graham@protonmail.com>
2024-05-09 10:44:35 -0600

    added better targeting heuristic

--- src/dispatcher.js
+++ src/dispatcher.js
@@ -38,6 +38,43 @@ export async function main(ns) {
     }
 }
 
+/** @param {NS} ns */
+// Returns a weight that can be used to sort servers by hack desirability
+function weight(ns, server) {
+	if (!server) return 0;
+
+	// Don't ask, endgame stuff
+	if (server.startsWith('hacknet-node')) return 0;
+
+	// Get the player information
+	let player = ns.getPlayer();
+
+	// Get the server information
+	let so = ns.getServer(server);
+
+	// Set security to minimum on the server object (for Formula.exe functions)
+	so.hackDifficulty = so.minDifficulty;
+
+	// We cannot hack a server that has more than our hacking skill so these have no value
+	if (so.requiredHackingSkill > player.skills.hacking) return 0;
+
+	// Default pre-Formulas.exe weight. minDifficulty directly affects times, so it substitutes for min security times
+	let weight = so.moneyMax / so.minDifficulty;
+
+	// If we have formulas, we can refine the weight calculation
+	if (ns.fileExists('Formulas.exe')) {
+		// We use weakenTime instead of minDifficulty since we got access to it, 
+		// and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
+		weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
+	}
+	else
+		// If we do not have formulas, we can't properly factor in hackchance, so we lower the hacking level tolerance by half
+		if (so.requiredHackingSkill > player.skills.hacking / 2)
+			return 0;
+
+	return weight;
+}
+
   /** @param {NS} ns */
 function get_flags(ns) {
     let debugmode = ns.peek(25575);
@@ -104,6 +141,12 @@ function get_flags(ns) {
     return network;
 }
 
+function advanced_calculate_optimal_targets(ns, targets, topx) {
+    let servers = targets.filter(s => weight(ns, s) > 0);
+    servers.sort((a, b) => weight(ns, b) - weight(ns, a));
+    return servers.slice(0,topx)
+}
+
 /** @param {NS} ns */
 function calculate_optimal_targets(ns, targets, topx) {
     // As a rule of thumb, your hacking target should be the server 
@@ -141,13 +184,13 @@ function parse_orders(ns, threadTargets) {
     let topx = 0;
     let targets = flags.targets;
     if (targets[0] == "NULL PORT DATA") {
-        return calculate_optimal_targets(ns, threadTargets, 5);
+        return advanced_calculate_optimal_targets(ns, threadTargets, 5);
     }
     if (targets[0] == "top") {
         topx = targets[1];
-        return calculate_optimal_targets(ns, threadTargets, topx);
+        return advanced_calculate_optimal_targets(ns, threadTargets, topx);
     }
-    return calculate_optimal_targets(ns, targets, targets.length);
+    return advanced_calculate_optimal_targets(ns, targets, targets.length);
 }
 
 /** @param {NS} ns */
---
+++ src/xsinx-optimal-target.js
@@ -0,0 +1,65 @@
+/** @param {NS} ns */
+export async function main(ns) {
+	const [runCount = 0, scriptName = 'v1.js', pct = 0.25] = ns.args;
+
+	let servers = GetAllServers(ns).filter(s => Weight(ns, s) > 0);
+	servers.sort((a, b) => Weight(ns, b) - Weight(ns, a));
+
+	let ran = 0;
+
+	ns.tprint('Best servers: ');
+	for (let server of servers) {
+		ns.tprint(server.padEnd(20) + ': ' + Weight(ns, server));
+		if (ran < runCount) {
+			let pid = ns.run(scriptName, 1, server, pct);
+			if (pid != 0) {
+				ns.tprint('INFO: Started ' + scriptName + ' on ' + server);
+				ran++;
+			}
+		}
+	}
+}
+
+/** @param {NS} ns */
+// Returns a weight that can be used to sort servers by hack desirability
+function Weight(ns, server) {
+	if (!server) return 0;
+
+	// Don't ask, endgame stuff
+	if (server.startsWith('hacknet-node')) return 0;
+
+	// Get the player information
+	let player = ns.getPlayer();
+
+	// Get the server information
+	let so = ns.getServer(server);
+
+	// Set security to minimum on the server object (for Formula.exe functions)
+	so.hackDifficulty = so.minDifficulty;
+
+	// We cannot hack a server that has more than our hacking skill so these have no value
+	if (so.requiredHackingSkill > player.skills.hacking) return 0;
+
+	// Default pre-Formulas.exe weight. minDifficulty directly affects times, so it substitutes for min security times
+	let weight = so.moneyMax / so.minDifficulty;
+
+	// If we have formulas, we can refine the weight calculation
+	if (ns.fileExists('Formulas.exe')) {
+		// We use weakenTime instead of minDifficulty since we got access to it, 
+		// and we add hackChance to the mix (pre-formulas.exe hack chance formula is based on current security, which is useless)
+		weight = so.moneyMax / ns.formulas.hacking.weakenTime(so, player) * ns.formulas.hacking.hackChance(so, player);
+	}
+	else
+		// If we do not have formulas, we can't properly factor in hackchance, so we lower the hacking level tolerance by half
+		if (so.requiredHackingSkill > player.skills.hacking / 2)
+			return 0;
+
+	return weight;
+}
+
+// Recursive network scan, compressed
+export function GetAllServers(ns, root = 'home', found = []) {
+	found.push(root);
+	for (const server of root == 'home' ? ns.scan(root) : ns.scan(root).slice(1)) GetAllServers(ns, server, found);
+	return found;
+}
